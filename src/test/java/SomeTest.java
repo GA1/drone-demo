8f282092c285052c7c3c6b730f7b923dcbf99d7c
Kamil Nguyen Van <knguyen@zinio.com>
2016-05-24 11:26:40 +0200

    pierwszy commit

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by knguyen on 5/24/16.
+ */
+public class SomeTest {
+
+    @Test
+    public void pierwszyTest() {
+        assertTrue(2==2);
+    }
+}
